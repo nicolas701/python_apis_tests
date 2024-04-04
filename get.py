2632fb4fdc2e06e6d32e9e5f238b3ee8c837e173
Nicolás Ruggiero <nicolas.ruggiero@semperti.com>
2024-04-04 12:21:45 -0300

    tests get

--- get.py
+++ get.py
@@ -1,7 +1,21 @@
 import requests
 
 if __name__ == '__main__':
-    url = 'https://www.google.com.ar/'
-    response = requests.get(url)
+    url = 'http://httpbin.org/get'
+    args = { 'nombre':'nico', 'curso':'python' }
 
-    print(response)
+    response = requests.get(url, params=args)
+    print(response.url)
+
+    if response.status_code == 200:
+        content = response.content
+        print(content)
+
+        #print(response.content) # Imprimo en consola la página
+
+        # Pasra guardar la página en un archivo html
+        # content = response.content
+        # file = open ('pagina.html', 'wb')
+        # file.write(content)
+        # file.close()
+
